e1cf95e3f899c5435529e25295dc953197fbd174
Daniel Marrable <marrabld@gmail.com>
2012-09-11 11:38:02 +0800

    updated importTools.py

--- src/importTools.py
+++ src/importTools.py
@@ -1,8 +1,10 @@
 import catamiWebPortal
 import os
 from django.core import serializers
-from Force.models import users
+from Force.models import *
 import json
+from django.contrib.gis.geos import GEOSGeometry
+
 #==================================================#
 # Created Dan marrable 5/09/2012
 # d.marrable@ivec.org
@@ -14,6 +16,9 @@ import json
 # files directly from a console - for now.  Files
 # include .json .xml .yaml but must conform to 
 # catami metadata datamodel
+#--------------------------------------------------#
+# @todo Include more import file formats.
+# @todo Include more deployment types
 #==================================================#
 
 class importMetaData():
@@ -26,9 +31,59 @@ class importMetaData():
         '''
         catamiWebPortal.logging.info("Importing metadata from " + file)
         fileName, fileExtension = os.path.splitext(file)
+                
+        if fileExtension == '.json':
+            data = json.load(open(file))
+            dataModel = users(**data)
+            dataModel.full_clean()
+            dataModel.save()
+        else:
+            catamiWebPortal.logged.error("No supported fileformat found.  Data not logged :: file extension :: " + fileExtension)
+
+            
+
+    @staticmethod
+    def importCampaignFromFile(file):
+        '''
+        @brief This function reads in a metadta file that includes campaign information.
+        @param file The file that holds the metata data.  formats include .json todo:-> .xml .yaml
+        '''
+        catamiWebPortal.logging.info("Importing metadata from " + file)
+        fileName, fileExtension = os.path.splitext(file)
         
         
         if fileExtension == '.json':
             data = json.load(open(file))
-            userModel = users(**data)
-            userModel.save()
+            dataModel = campaign(**data)
+            dataModel.full_clean()
+            dataModel.save()
+        else:
+            catamiWebPortal.logged.error("No supported fileformat found.  Data not logged :: file extension :: " + fileExtension)
+
+            
+
+    @staticmethod
+    def importDeploymentFromFile(file):
+        '''
+        @brief This function reads in a metadta file that includes campaign information.  Destinction between deployment types is made on the fine name.  <type><deployment>.<supported text> auvdeployment.json
+        @param file The file that holds the metata data.  formats include .json todo:-> .xml .yaml
+        '''
+        catamiWebPortal.logging.info("Importing metadata from " + file)
+        fileName, fileExtension = os.path.splitext(file)
+
+        if fileExtension == '.json':
+            if os.path.basename(fileName.upper()) == 'AUVDEPLOYMENT':
+                catamiWebPortal.logging.info("Found valid deployment file")
+                data = json.load(open(file))
+                dataModel = auvDeployment(**data)
+                try:
+                    dataModel.full_clean()
+                except ValidationError as e:
+                    catamiWebPortal.logging.warning("Possible validation error :: " + e.errno + e.strerror)
+
+                    
+                dataModel.save()
+            else:
+                catamiWebPortal.logging.error("No supported filname found.  Data not logged :: filename :: " + file)
+        else:
+            catamiWebPortal.logged.error("No supported fileformat found.  Data not logged :: file extension :: " + fileExtension)
